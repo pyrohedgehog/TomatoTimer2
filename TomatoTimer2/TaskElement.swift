ac3928d45a99260a35dbd4f72b8f53559ca49a7c
Pyro Hedgehog <jpwilmsmeyer@gmail.com>
2020-06-23 11:25:35 -0400

    Saftey Push
    About to remove redundant page to implement recursive agendaViewSystem

--- TomatoTimer2/TaskElement.swift
+++ TomatoTimer2/TaskElement.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+import UIKit
+
+//protocol TaskElement{
+//
+//}
+//extension TaskElement {
+//
+//
+//}
+class TaskElementCell : UITableViewCell{
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+    func setText(tomato: Tomato){
+        //  nameLabel.text = tomato.name
+        textLabel?.text = tomato.name
+        detailTextLabel?.text = tomato.description
+    }
+    func setText(agenda : TomatoHandler){
+        textLabel?.text = agenda.name
+        detailTextLabel?.text = agenda.moreInfo
+    }
+}
